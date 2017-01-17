fb2b40f172ed8b00c603f95f313aa560521bd485
Spencer Simons <spencersimons@Spencers-MacBook-Pro.local>
2017-01-16 21:20:23 -0700

    finished basic header template for all site pages

---
+++ js.js
@@ -0,0 +1,8 @@
+function showMobileNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
