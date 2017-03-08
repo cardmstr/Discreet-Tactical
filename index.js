cc566364bab050aa5c79fc1e4879ac66242c87a7
Spencer Simons <spencersimons@Spencers-MacBook-Pro.local>
2017-03-07 23:54:40 -0700

    random commit to try to fix my states issue

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var app = express();
 app.use(express.static('site'));
 app.use(bodyParser.json());
 
+app.get('/contact', function(req, res){
+  console.log(req);
+})
 
 app.listen(3000, function(){
   console.log('app running on port ',3000);
