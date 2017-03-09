f54b77c76b034496b16eb43ed5a68e77e4d8e297
Spencer Simons <spencersimons@Spencers-MacBook-Pro.local>
2017-03-08 20:26:13 -0700

    final test, better flippin work

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+app.set('port', ('process.env.PORT' || 5000))
+
 app.use(express.static('site'));
 app.use(bodyParser.json());
 
-app.get('/contact', function(req, res){
-  console.log(req);
+app.get('/', function(req, res){
+  response.render('/');
 })
+
 
 app.listen(3000, function(){
   console.log('app running on port ',3000);
