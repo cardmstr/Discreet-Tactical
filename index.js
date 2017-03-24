d00eccfda059503de18ec8c0069aa412e6938151
Spencer Simons <spencersimons@Spencers-MacBook-Pro.local>
2017-03-23 20:29:02 -0600

    moved contactForm route callback to routesCtrl

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var nodemailer = require('nodemailer');
 var secrets = require('./secrets');
 var routeCtrl = require('./site/states/routesCtrl');
 
@@ -15,45 +14,11 @@ app.use(bodyParser.urlencoded({ //needed for ContactForm
     extended: true
 }));
 
-app.get('/', function(req, res){ //needed for Heroku
-  response.render('/');
-})
 
-app.post('/contactSubmit', function(req,res){
-  console.log(req);
-  var data=req.body;
-  console.log(req.body);
-
-  var smtpTransport = nodemailer.createTransport({
-     service: "Gmail",
-     auth: {
-     user: secrets.emailID,
-     pass: secrets.emailPass
-  }});
-
-  var htmlMsg = "Name: " + req.body.name + "<br>Phone: " + req.body.phone + "<br>Email: " + req.body.email + "<br>Message: " + req.body.message;
-console.log(htmlMsg);
-   smtpTransport.sendMail({
-   from: "Discreet Form <" + secrets.emailID +">",
-   to: "Spencer Simons <spencersimons09@gmail.com>",
-   subject: "Discreet Webform from " + req.body.name,
-   html: htmlMsg
-    }, function(error, response){
-         if(error){
-           res.render('contact', { title: 'Discreet Tactical - Contact', msg: 'Error occured, message not sent.', err: true, page: 'contact' })
-           console.log(error);
-        }else{
-           res.render('contact', { title: 'Discreet Tactical - Contact', msg: 'Message sent! Thank you.', err: false, page: 'contact' })
-           console.log("Message sent: " + res.message);
-       }
-
-   smtpTransport.close();
-    });
-    res.send('contactSubmit ran');
-})
+app.get('/', routeCtrl.getHome);
 
 
-
+app.post('/contactSubmit', routeCtrl.contactSubmit);
 
 app.listen((process.env.PORT || 5000), function(){
   console.log('app running on port ',(process.env.PORT || 5000));
--- site/states/routesCtrl.js
+++ site/states/routesCtrl.js
@@ -1,3 +1,38 @@
+var nodemailer = require('nodemailer');
+var secrets = require('../../secrets');
+
 module.exports = {
-  contactSubmit: ""
+  contactSubmit: function(req,res){
+      var data=req.body;
+
+      var smtpTransport = nodemailer.createTransport({
+         service: "Gmail",
+         auth: {
+         user: secrets.emailID,
+         pass: secrets.emailPass
+      }});
+
+      var htmlMsg = "Name: " + req.body.name + "<br>Phone: " + req.body.phone + "<br>Email: " + req.body.email + "<br>Message: " + req.body.message;
+
+       smtpTransport.sendMail({
+       from: "Discreet Form <" + secrets.emailID +">",
+       to: secrets.emailTo,
+       subject: "Discreet Webform from " + req.body.name,
+       html: htmlMsg
+        }, function(error, response){
+             if(error){
+               res.render('contact', { title: 'Discreet Tactical - Contact', msg: 'Error occured, message not sent.', err: true, page: 'contact' })
+               console.log(error);
+            }else{
+               res.render('contact', { title: 'Discreet Tactical - Contact', msg: 'Message sent! Thank you.', err: false, page: 'contact' })
+               console.log("Message sent: " + res.message);
+           }
+
+       smtpTransport.close();
+        });
+        res.send('contactSubmit ran');
+    },
+    getHome: function(req, res){
+      response.render('/');
+    }
 }
